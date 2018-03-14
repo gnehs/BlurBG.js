a75d483f5f3608dd4f01ded0ea14278109f9296c
GNEHS <jayuiop@gmail.com>
2018-03-14 20:01:59 +0800

    先弄個架構

---
+++ BlurBG.js
@@ -0,0 +1,12 @@
+function BlurBG(sourceImg, blur = '4px', class = 'BlurBG', opacity = 50) {
+    /*
+    sourceImg 原始圖片
+    blur 模糊程度
+    class 輸出的 Class
+        .BlurBG{
+             background-image:url("data:image/png;base64, iVBOR/w38GIA...")
+        }
+    opacity 輸出圖片的透明度，可以搭配背景顏色使用
+    */
+    // 好懶喔
+}
