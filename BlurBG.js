b231989f6c29f1dbfcd5e6100e2ef72766af86d0
GNEHS <jayuiop@gmail.com>
2018-03-17 12:59:56 +0800

    好ㄌ

--- BlurBG.js
+++ BlurBG.js
@@ -1,12 +1,47 @@
-function BlurBG(sourceImg, blur = '4px', class = 'BlurBG', opacity = 50) {
-    /*
-    sourceImg 原始圖片
-    blur 模糊程度
-    class 輸出的 Class
-        .BlurBG{
-             background-image:url("data:image/png;base64, iVBOR/w38GIA...")
-        }
-    opacity 輸出圖片的透明度，可以搭配背景顏色使用
+function BlurBG(Img_source, Img_filter, CSS_class) {
+    /*  sourceImg 原始圖片
+        blur 模糊程度
+        class 輸出的 Class
+            .BlurBG{
+                background-image:url("data:image/png;base64, iVBOR/w38GIA...")
+            }
+        opacity 輸出圖片的透明度，可以搭配背景顏色使用
     */
+    //整理拿到的數據，沒有的話給預設值
+    Img_filter = Img_filter || "blur(5px) opacity(50%)";
+    CSS_class = CSS_class || "BlurBG";
     // 好懶喔
+
+    var img = new Image();
+    img.src = Img_source;
+    img.setAttribute("crossOrigin", 'Anonymous')
+    img.onload = function() {
+        var idRandom = Math.floor(Math.random() * 10000);
+        imgHeight = img.height;
+        imgWidth = img.width;
+        console.log('image size: %sx%s', imgWidth, imgHeight)
+
+        // Create Canvas
+        var createCanvas = document.createElement("canvas");
+        createCanvas.setAttribute('id', 'blur_canvas' + idRandom);
+        createCanvas.setAttribute('style', 'display:none');
+        createCanvas.setAttribute('width', imgWidth);
+        createCanvas.setAttribute('height', imgHeight);
+        document.body.appendChild(createCanvas);
+
+        // Blur
+        var canvas = document.getElementById('blur_canvas' + idRandom);
+        var ctx = canvas.getContext('2d');
+        ctx.filter = Img_filter;
+        ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
+        // 產生 Style 供套用
+        var PngBase64 = canvas.toDataURL();
+        var css = '.' + CSS_class + '{background-image:url(' + PngBase64 + ')}'
+
+        var createClass = document.createElement("style");
+        createClass.setAttribute('type', 'text/css');
+        createClass.appendChild(document.createTextNode(css));
+
+        document.head.appendChild(createClass)
+    };
 }
