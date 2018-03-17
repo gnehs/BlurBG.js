a85b2553307681ee690454968878713eabe7baba
GNEHS <jayuiop@gmail.com>
2018-03-17 13:54:28 +0800

    sessionStorage 功能

--- BlurBG.js
+++ BlurBG.js
@@ -7,46 +7,42 @@ function BlurBG(Img_source, Img_filter, CSS_class) {
             }
         opacity 輸出圖片的透明度，可以搭配背景顏色使用
     */
-    //看看 sessionStorage 有沒有圖片
-    if (window.sessionStorage["Blur_" + Img_source]) {
-        createClass(window.sessionStorage["Blur_" + Img_source], CSS_class)
-    }
     //整理拿到的數據，沒有的話給預設值
     Img_filter = Img_filter || "blur(5px) opacity(50%)";
     CSS_class = CSS_class || "BlurBG";
+    //看看 sessionStorage 有沒有圖片
+    if (window.sessionStorage["Blur_" + Img_source] && window.sessionStorage["filter_" + Img_source] == Img_filter) {
+        createClass(window.sessionStorage["Blur_" + Img_source], CSS_class)
+        return
+    }
     // 好懶喔
     var img = new Image();
     img.src = Img_source;
     img.setAttribute("crossOrigin", 'Anonymous')
     img.onload = function() {
-        var idRandom = Math.floor(Math.random() * 10000);
         imgHeight = img.height;
         imgWidth = img.width;
-        console.log('image size: %sx%s', imgWidth, imgHeight)
 
         // Create Canvas
-        var createCanvas = document.createElement("canvas");
-        createCanvas.setAttribute('id', 'blur_canvas' + idRandom);
-        createCanvas.setAttribute('style', 'display:none');
-        createCanvas.setAttribute('width', imgWidth);
-        createCanvas.setAttribute('height', imgHeight);
-        document.body.appendChild(createCanvas);
+        var canvas = document.createElement("canvas");
+        canvas.setAttribute('width', imgWidth);
+        canvas.setAttribute('height', imgHeight);
 
         // Blur
-        var canvas = document.getElementById('blur_canvas' + idRandom);
         var ctx = canvas.getContext('2d');
         ctx.filter = Img_filter;
         ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
         // 產生 Style 供套用
-        var PngBase64 = canvas.toDataURL("image/png");
+        var PngBase64 = canvas.toDataURL();
         createClass(PngBase64, CSS_class);
         // 存到 sessionStorage
         window.sessionStorage["Blur_" + Img_source] = PngBase64;
+        window.sessionStorage["filter_" + Img_source] = Img_filter; //如果這個參數不同，將重新產生圖片
     };
+
 }
 
 function createClass(img, CSS_class) {
-
     var css = '.' + CSS_class + '{background-image:url(' + img + ')}'
 
     var createClass = document.createElement("style");
