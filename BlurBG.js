d569ecc21fcc256e18ee5c2a79eb3a46c05a3ca7
gnehs <jayuiop@gmail.com>
2019-06-30 18:52:54 +0800

    更新 js

--- BlurBG.js
+++ BlurBG.js
@@ -1,39 +1,30 @@
-function BlurBG(Img_source, Img_filter, CSS_class) {
-
-    //整理拿到的數據，沒有的話給預設值
-    Img_filter = Img_filter || "blur(5px) opacity(40%)";
-    CSS_class = CSS_class || "BlurBG";
-
+function BlurBG(Img_source, Img_filter = "blur(5px) opacity(40%)", CSS_class = "BlurBG") {
     //看看 sessionStorage 有沒有圖片
     if (window.sessionStorage[b64EncodeUnicode("Blur_" + Img_source)] && window.sessionStorage[b64EncodeUnicode("filter_" + Img_source)] == Img_filter) {
         createClass(window.sessionStorage[b64EncodeUnicode("Blur_" + Img_source)], CSS_class, Img_filter)
     } else {
         blurImg(Img_source, Img_filter, CSS_class)
     }
-
 }
 
 function blurImg(Img_source, Img_filter, CSS_class) {
-
-
     // Create Canvas
-    var canvas = document.createElement("canvas");
-
+    let canvas = document.createElement("canvas");
     // 好懶喔
-    var img = new Image();
+    let img = new Image();
     img.setAttribute('crossOrigin', 'anonymous');
     img.src = Img_source;
-    img.onload = function() {
-        var imgHeight = 700
-        var imgWidth = 700 * (img.width / img.height)
+    img.onload = function () {
+        let imgHeight = 700
+        let imgWidth = 700 * (img.width / img.height)
         canvas.setAttribute('width', imgWidth);
         canvas.setAttribute('height', imgHeight);
         // Blur
-        var ctx = canvas.getContext('2d');
+        let ctx = canvas.getContext('2d');
         ctx.filter = Img_filter;
         ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
         // Base64
-        var PngBase64 = canvas.toDataURL();
+        let PngBase64 = canvas.toDataURL();
         // 產生 Style 供套用
         createClass(PngBase64, CSS_class, Img_filter);
         // 存到 sessionStorage
@@ -43,9 +34,9 @@ function blurImg(Img_source, Img_filter, CSS_class) {
 }
 
 function createClass(img, CSS_class, Img_filter) {
-    var css = '.' + CSS_class + '{background-image:url(' + img + ');'
+    let css = `.${CSS_class}{background-image:url('${img}');}`
 
-    var createClass = document.createElement("style");
+    let createClass = document.createElement("style");
     createClass.setAttribute('type', 'text/css');
     createClass.appendChild(document.createTextNode(css));
 
@@ -54,7 +45,7 @@ function createClass(img, CSS_class, Img_filter) {
 
 function b64DecodeUnicode(str) {
     // Going backwards: from bytestream, to percent-encoding, to original string.
-    return decodeURIComponent(atob(str).split('').map(function(c) {
+    return decodeURIComponent(atob(str).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 }
